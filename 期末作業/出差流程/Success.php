952a9440f3645adc71f03eca5ee0f49f149a06d3
kuasmisbug <ray19990613@gmail.com>
2020-01-02 02:29:16 +0800

    期末作業 - 出差流程處理完畢

---
+++ 期末作業/出差流程/Success.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+include("config.php");
+$event_time = date('Y-m-d H:i:s');
+
+if(isset($_GET['DocNo'])){
+    $conn = new mysqli($host, $account, $password, $database);
+    $result = $conn->query('INSERT INTO easyflow (DocNo,Status,event_time) Values ("'. $_GET['DocNo'] .'", 3 ,"'. $event_time .'")');
+    echo "更新狀態：處理完畢";
+}
