392fcd313ecbc5089143096dd70dd005c4b0c5c6
kuasmisbug <ray19990613@gmail.com>
2020-01-02 02:22:55 +0800

    期末作業 - 離職流程(修正亂碼問題)

--- 期末作業/離職流程/index.php
+++ 期末作業/離職流程/index.php
@@ -3,7 +3,7 @@
 
 <head>
   <!-- Required meta tags -->
-  <meta charset="utf8-">
+  <meta charset="utf8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
