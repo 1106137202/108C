1834defdebaa3af734b7acd152007fc138805d2b
kuasmisbug <ray19990613@gmail.com>
2020-01-02 02:24:17 +0800

    期末作業 - 離職流程不予受理(修正亂碼問題)

--- 期末作業/離職流程/Failure.php
+++ 期末作業/離職流程/Failure.php
@@ -5,7 +5,7 @@ $event_time = date('Y-m-d H:i:s');
 
 if(isset($_GET['DocNo'])){
     $conn = new mysqli($host, $account, $password, $database);
-    $result = $conn->query('INSERT INTO easyflow (DocNo,Status,event_time) Values ("'. $_GET['DocNo'] .'","不予受理","'. $event_time .'")');
+    $result = $conn->query('INSERT INTO easyflow (DocNo,Status,event_time) Values ("'. $_GET['DocNo'] .'", 2 ,"'. $event_time .'")');
     echo "更新狀態：不予受理";
 }
 ?>
