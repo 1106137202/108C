907ac955c80101d50f8dfc6ecb084a0fb78f9f7b
kuasmisbug <ray19990613@gmail.com>
2020-01-02 02:23:48 +0800

    期末作業 - 離職流程處理完畢(修正亂碼問題)

--- 期末作業/離職流程/Success.php
+++ 期末作業/離職流程/Success.php
@@ -5,6 +5,6 @@ $event_time = date('Y-m-d H:i:s');
 
 if(isset($_GET['DocNo'])){
     $conn = new mysqli($host, $account, $password, $database);
-    $result = $conn->query('INSERT INTO easyflow (DocNo,Status,event_time) Values ("'. $_GET['DocNo'] .'","處理完畢","'. $event_time .'")');
+    $result = $conn->query('INSERT INTO easyflow (DocNo,Status,event_time) Values ("'. $_GET['DocNo'] .'", 3 ,"'. $event_time .'")');
     echo "更新狀態：處理完畢";
 }
